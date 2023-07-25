d103596daec805d59180f9855cd21ffa289272d1
BlueTree-Na <ncs990808@gmail.com>
2023-07-25 20:09:49 +0900

    refactor (InitData) : init data에서 DB로 전환

--- src/main/java/com/ncs/demo/config/InitData.java
+++ src/main/java/com/ncs/demo/config/InitData.java
@@ -20,34 +20,34 @@
 //
 //    @PostConstruct
 //    public void init(){
+////
+////        Member member1 = new Member("nacs", "1234", "나청송", "BT");
+////        memberRepository.memberSave(member1);
 //
-//        Member member1 = new Member("nacs", "1234", "나청송", "BT");
-//        memberRepository.memberSave(member1);
 //
-//
-//        Board board1 = new Board(member1.getMemberManageSeq(), "HTML1", "zyzyzyy", "2023-06-14", member1.getNickName(), "Front-End");
+//        Board board1 = new Board(2L, "HTML1", "zyzyzyy", "2023-06-14", "나청송", "Front-End");
 //        boardRepository.boardSave(board1);
-//        Board board2 = new Board(member1.getMemberManageSeq(), "HTML2", "1234", "2023-06-14", member1.getNickName(), "Front-End");
+//        Board board2 = new Board(2L, "HTML2", "1234", "2023-06-14", "나청송", "Front-End");
 //        boardRepository.boardSave(board2);
 //
-//        Board board3 = new Board(member1.getMemberManageSeq(), "MVC1", "1234", "2023-06-14", member1.getNickName(), "Back-End");
+//        Board board3 = new Board(2L, "MVC1", "1234", "2023-06-14", "나청송", "Back-End");
 //        boardRepository.boardSave(board3);
-//        Board board4 = new Board(member1.getMemberManageSeq(), "MVC2", "1234", "2023-06-14", member1.getNickName(), "Back-End");
+//        Board board4 = new Board(2L, "MVC2", "1234", "2023-06-14", "나청송", "Back-End");
 //        boardRepository.boardSave(board4);
 //
-//        Board board5 = new Board(member1.getMemberManageSeq(), "전처리1", "1234", "2023-06-14", member1.getNickName(), "AI");
+//        Board board5 = new Board(2L, "전처리1", "1234", "2023-06-14", "나청송", "AI");
 //        boardRepository.boardSave(board5);
-//        Board board6 = new Board(member1.getMemberManageSeq(), "전처리2", "1234", "2023-06-14", member1.getNickName(), "AI");
+//        Board board6 = new Board(2L, "전처리2", "1234", "2023-06-14", "나청송", "AI");
 //        boardRepository.boardSave(board6);
 //
-//        Board board7 = new Board(member1.getMemberManageSeq(), "MySQL1", "1234", "2023-06-14", member1.getNickName(), "DB");
+//        Board board7 = new Board(2L, "MySQL1", "1234", "2023-06-14", "나청송", "DB");
 //        boardRepository.boardSave(board7);
-//        Board board8 = new Board(member1.getMemberManageSeq(), "MySQL2", "1234", "2023-06-14", member1.getNickName(), "DB");
+//        Board board8 = new Board(2L, "MySQL2", "1234", "2023-06-14", "나청송", "DB");
 //        boardRepository.boardSave(board8);
 //
-//        Board board9 = new Board(member1.getMemberManageSeq(), "정렬1", "1234", "2023-06-14", member1.getNickName(), "Algorithm");
+//        Board board9 = new Board(2L, "정렬1", "1234", "2023-06-14", "나청송", "Algorithm");
 //        boardRepository.boardSave(board9);
-//        Board board10 = new Board(member1.getMemberManageSeq(), "정렬2", "1234", "2023-06-14", member1.getNickName(), "Algorithm");
+//        Board board10 = new Board(2L, "정렬2", "1234", "2023-06-14", "나청송", "Algorithm");
 //        boardRepository.boardSave(board10);
 //
 //    }
